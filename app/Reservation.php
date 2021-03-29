cd49c74ae97e611c4e0c1edea3d85f4263394b51
先規　勇樹 <yukisaki80@gmail.com>
2021-03-30 08:03:28 +0900

    21問目解答

--- app/Reservation.php
+++ app/Reservation.php
@@ -18,6 +18,7 @@ class Reservation extends Model
      */
     public function user()
     {
+        return $this->belongsTo('App\User');
     }
 
     /**
--- database/seeds/ReservationsTableSeeder.php
+++ database/seeds/ReservationsTableSeeder.php
@@ -11,5 +11,69 @@ class ReservationsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('reservations')->insert([
+            'reservation_number' => '000001',
+            'title' => 'テストA1',
+            'user_id' => '2',
+            'meeting_room_id' => '1',
+            'start_time' => '2021-03-20 10:00:00',
+            'end_time' => '2021-03-20 14:00:00',
+            'is_approved' => true,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservations')->insert([
+            'reservation_number' => '000002',
+            'title' => 'テストB1',
+            'user_id' => '2',
+            'meeting_room_id' => '2',
+            'start_time' => '2021-04-02 12:00:00',
+            'end_time' => '2021-04-02 15:00:00',
+            'is_approved' => true,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservations')->insert([
+            'reservation_number' => '000003',
+            'title' => 'テストC1',
+            'user_id' => '2',
+            'meeting_room_id' => '3',
+            'start_time' => '2021-04-03 14:00:00',
+            'end_time' => '2021-04-03 15:00:00',
+            'is_approved' => true,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservations')->insert([
+            'reservation_number' => '000004',
+            'title' => 'テストC2',
+            'user_id' => '2',
+            'meeting_room_id' => '3',
+            'start_time' => '2021-04-03 16:00:00',
+            'end_time' => '2021-04-03 17:00:00',
+            'is_approved' => false,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservations')->insert([
+            'reservation_number' => '000005',
+            'title' => 'テストC3',
+            'user_id' => '2',
+            'meeting_room_id' => '3',
+            'start_time' => '2021-04-03 18:00:00',
+            'end_time' => '2021-04-03 19:00:00',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reservations')->insert([
+            'reservation_number' => '000006',
+            'title' => 'テストC4',
+            'user_id' => '3',
+            'meeting_room_id' => '1',
+            'start_time' => '2021-04-05 18:00:00',
+            'end_time' => '2021-04-05 19:00:00',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
     }
 }
