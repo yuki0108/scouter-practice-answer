4383baaa37be2d1febcef3aaebe868b0a0c3544f
先規　勇樹 <yukisaki80@gmail.com>
2021-03-30 00:11:16 +0900

    15問答え

--- app/Http/Controllers/MeetingRoomController.php
+++ app/Http/Controllers/MeetingRoomController.php
@@ -21,11 +21,34 @@ class MeetingRoomController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function index()
+    public function index(Request $request)
     {
+        if (Auth::user()->is_administrator) {
+            // 管理者の場合
+            $meetingRooms = MeetingRoom::get();
+            return view('meeting_room_index',
+                [
+                    'meetingRooms' => $meetingRooms
+                ]);
+
+        } else {
+            // 一般ユーザーの場合
+            return redirect('/');
+        }
     }
 
     public function create(){
+        if (Auth::user()->is_administrator) {
+            // 管理者の場合
+            return view('meeting_room_create',
+                [
+                    'meetingRooms' => MeetingRoom::get(),
+                    'items' => Item::get(),
+                ]);
+        } else {
+            // 一般ユーザーの場合
+            return redirect('/');
+        }
     }
 
     /**
@@ -65,8 +88,19 @@ class MeetingRoomController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function edit()
+    public function edit(Request $request)
     {
+        if (Auth::user()->is_administrator) {
+            // 管理者の場合
+            $meetingRoom = MeetingRoom::find($request->id);
+            return view('meeting_room_edit',
+                [
+                    'room' => $meetingRoom,
+                ]);
+        } else {
+            // 一般ユーザーの場合
+            return redirect('/');
+        }
     }
 
     /**
@@ -74,8 +108,28 @@ class MeetingRoomController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function update()
+    public function update(Request $request)
     {
+        if (Auth::user()->is_administrator) {
+            // 管理者の場合
+            $request->validate([
+                'name' => 'required|max:255',
+                'max_use_hour' => 'nullable|int',
+                'needs_approval' => 'required|boolean',
+            ],
+            [],
+            [
+                'name' => '会議室名',
+            ]);
+
+            MeetingRoom::where('id', $request->id)
+                ->update(['id' => $request->id,
+                    'name' => $request->name,
+                    'max_use_hour' => $request->max_use_hour,
+                    'needs_approval' => $request->needs_approval]);
+        }
+
+        return redirect(route('rooms.index'));
     }
 
     /**
@@ -83,7 +137,17 @@ class MeetingRoomController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function delete()
+    public function delete(Request $request)
     {
+        if (Auth::user()->is_administrator) {
+            // 管理者の場合
+            $room = MeetingRoom::find($request->id);
+            $room->delete();
+
+            // 削除した会議室の予約も削除
+            $reservations = Reservation::where('meeting_room_id', $request->id);
+            $reservations->delete();
+        }
+        return redirect(route('rooms.index'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,17 +25,35 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function(){
     Route::get('/' , 'HomeController@index' );
     Route::get('/home' , 'HomeController@index' );
+    Route::get('/reservations/index' , 'ReservationController@index' )
+        ->name('reservations.index');
+    Route::get('/reservations/create' , 'ReservationController@create' )
+        ->name('reservations.create');
     Route::post('/reservations/store' , 'ReservationController@store' )
         ->name('reservations.store');
+    Route::get('/reservations/edit/{id}', 'ReservationController@edit')
+        ->name('reservations.edit');
+    Route::put('/reservations/update', 'ReservationController@update')
+        ->name('reservations.update');
+    Route::get('/reservations/delete/{id}', 'ReservationController@delete')
+        ->name('reservations.delete');
     Route::get('/reservations/approve/{id}', 'ReservationController@approve')
         ->name('reservations.approve');
     Route::get('/reservations/reject/{id}', 'ReservationController@reject')
         ->name('reservations.reject');
 
+    Route::get('/rooms/index' , 'MeetingRoomController@index' )
+        ->name('rooms.index');
     Route::get('/rooms/create' , 'MeetingRoomController@create' )
         ->name('rooms.create');
     Route::post('/rooms/store' , 'MeetingRoomController@store' )
         ->name('rooms.store');
+    Route::get('/rooms/edit/{id}', 'MeetingRoomController@edit')
+        ->name('rooms.edit');
+    Route::put('/rooms/update', 'MeetingRoomController@update')
+        ->name('rooms.update');
+    Route::get('/rooms/delete/{id}', 'MeetingRoomController@delete')
+        ->name('rooms.delete');
 
     Route::get('/users/index' , 'UserController@index' )
         ->name('users.index');
